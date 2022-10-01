40656e2501eb8c94cb8b78a85f5cc4a1882ce78c
Bill Rossi <bassguitarbill@gmail.com>
2022-10-01 15:52:21 -0400

    Try to add the beginnings of pathing

--- src/Room.ts
+++ src/Room.ts
@@ -5,17 +5,30 @@ import images from './images.js';
 export default class Room {
     walls: Array<Wall> = [];
     doors: Array<Door> = [];
+    pathing: Array<Tile> = [];
     tiled: any;
 
     constructor(readonly game: Game, readonly path: string) {
         this.generateWallsFromTiled = this.generateWallsFromTiled.bind(this);
         this.generateDoorsFromTiled = this.generateDoorsFromTiled.bind(this);
+        this.generatePathingFromTiled = this.generatePathingFromTiled.bind(this);
         fetch(`maps/${path}.json`).then(rsp => rsp.json())
             .then(tiled => {
                 this.tiled = tiled;
                 this.generateWallsFromTiled(tiled);
                 this.generateDoorsFromTiled(tiled);
+                this.generatePathingFromTiled(tiled);
             });
+    }
+
+    generatePathingFromTiled(tiled: any) {
+        const pathingLayer = (tiled.layers as Array<any>).find(layer => layer.name === "pathing");
+        const width = pathingLayer.width;
+        (pathingLayer.data as Array<number>).forEach(
+            (tile, index) => this.addPathingTile(tile, index % width, Math.floor(index / width))
+        );
+        this.pathing.forEach(tile => tile.populateLinks(width));
+        console.log(this.pathing);
     }
 
     generateWallsFromTiled(tiled: any) {
@@ -26,7 +39,6 @@ export default class Room {
 
     generateDoorsFromTiled(tiled: any) {
         const doorLayer = (tiled.layers as Array<any>).find(layer => layer.name === "doors");
-        console.log(doorLayer)
         const doors: Array<any> = doorLayer.objects;
         doors.forEach(d => this.addDoor(d.x, d.y, d.width, d.height, d.properties));
     }
@@ -38,6 +50,7 @@ export default class Room {
         if ((window as any).debug) {
             this.walls.forEach(wall => wall.draw(ctx));
             this.doors.forEach(door => door.draw(ctx));
+            this.pathing.forEach(tile => tile.draw(ctx));
         }
     }
 
@@ -56,6 +69,10 @@ export default class Room {
 
     addDoor(x: number, y: number, w: number, h: number, props: any) {
         this.doors.push(new Door(this, x, y, w, h, props));
+    }
+
+    addPathingTile(tileIndex: number, x: number, y: number) {
+        this.pathing.push(new Tile(this, tileIndex, x, y));
     }
 }
 
@@ -99,3 +116,58 @@ class Wall {
         ctx.fillRect(x / 4, y / 4, w / 4, h / 4);
     }
 }
+
+class Tile {
+    links: Array<TileLink> = [];
+    constructor(
+        readonly room: Room,
+        readonly tileIndex: number,
+        readonly x: number,
+        readonly y: number,
+    ) { }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.fillStyle = this.tileIndex == 1 ? 'blue' : 'red';
+        ctx.fillRect(this.x * 12.5 + 165, this.y * 12.5 + 120, 5, 5);
+        this.links.forEach(l => {
+            ctx.beginPath()
+            ctx.moveTo(this.x * 12.5 + 165, this.y * 12.5 + 120)
+            ctx.lineTo(l.tile.x * 12.5 + 165, l.tile.y * 12.5 + 120)
+            ctx.stroke();
+        });
+    }
+
+    populateLinks(width: number) {
+        const { room, x, y } = this;
+        const { pathing } = room;
+
+        const populateLink = this.populateLink.bind(this, pathing);
+
+        if (y > 0) {
+            if (x > 0) populateLink((y - 1) * width + x - 1, 1.414)
+            populateLink((y - 1) * width + x, 1)
+            if (x < width - 1) populateLink((y - 1) * width + x + 1, 1.414)
+        }
+
+        if (x > 0) populateLink((y) * width + x - 1, 1)
+        if (x < width - 1) populateLink((y) * width + x + 1, 1)
+
+        if (y < pathing.length / width - 1) {
+            if (x > 0) populateLink((y + 1) * width + x - 1, 1.414)
+            populateLink((y + 1) * width + x, 1)
+            if (x < width - 1) populateLink((y + 1) * width + x + 1, 1.414)
+        }
+    }
+
+    populateLink(pathing: Array<Tile>, index: number, weight: number) {
+        if (pathing[index]) this.links.push(new TileLink(pathing[index], weight));
+    }
+}
+
+class TileLink {
+    constructor(
+        readonly tile: Tile,
+        readonly weight: number,
+    ) { }
+}
+
