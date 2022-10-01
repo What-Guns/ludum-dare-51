c5bcc58e1ef57435c0015351cfb4e6872268abed
Bill Rossi <bassguitarbill@gmail.com>
2022-10-01 09:11:55 -0400

    Lots of Tiled nonsense

---
+++ Walkable.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Walkable" tilewidth="86" tileheight="50" tilecount="10" columns="2">
+ <image source="../tiled/assets/Pixel Art Top Down - Basic/Texture/TX Tileset Grass.png" width="256" height="256"/>
+</tileset>
